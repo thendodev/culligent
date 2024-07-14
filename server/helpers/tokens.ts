7d84b304fb3454cd172d85b3f6a9ab6e165dc296
Thendo <thendo.tshikororo@typedev.co.za>
2024-07-14 13:23:50 +0200

    fix : change from hono to jsonwebtokens

--- server/helpers/tokens.ts
+++ server/helpers/tokens.ts
@@ -1,37 +1,28 @@
 import { envServer } from '@/global/envServer';
 import RefreshToken from '@/models/RefreshToken';
-import { sign } from 'hono/jwt';
+import jwt from 'jsonwebtoken';
 
 export const generateTokens = async (user: any) => {
   if (!envServer.JWT_SECRET || !envServer.JWT_REFRESH_SECRET)
     return Promise.reject('could not create token');
   try {
-    //create payload
-    const AccessPayload = {
-      sub: user._id,
-      exp: Math.floor(Date.now() / 1000) + 60 * 15,
-    };
-    const RefreshPayload = {
-      sub: user._id,
-      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
-    };
-
-    //create access token
-    const accessToken = await sign(
-      AccessPayload,
-      envServer.JWT_SECRET,
-      'HS256',
-    );
-
-    //create refresh token
-    const refreshToken = await sign(
-      RefreshPayload,
-      envServer.JWT_REFRESH_SECRET,
-      'HS256',
-    );
-    //remove pre-existing user refresh token
+    //delete old refresh tokens
     await RefreshToken.findOneAndDelete({
       user: user._id,
+    });
+
+    //create payload
+    const payload = {
+      sub: user._id,
+    };
+
+    //create a token
+
+    const accessToken = jwt.sign(payload, envServer.JWT_SECRET, {
+      expiresIn: '15m',
+    });
+    const refreshToken = jwt.sign(payload, envServer.JWT_REFRESH_SECRET, {
+      expiresIn: '7d',
     });
 
     //insert new refresh token
